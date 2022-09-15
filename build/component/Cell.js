8c5913a142b3a5dcffac7e76d24bd73acf2bfb28
arjun.bhatt <arjun.bhatt@joshtechnologygroup.com>
2022-09-15 11:08:06 +0530

    prepare script

--- build/component/Cell.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Cell = void 0;
-var Cell = function (_props) { return null; };
-exports.Cell = Cell;
-exports.Cell.displayName = 'Cell';
-//# sourceMappingURL=Cell.js.map
